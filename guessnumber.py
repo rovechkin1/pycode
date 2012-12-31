a8b5507d66725421b131da07e672c0de0e1b9d81
vladimir ovechkin <vladimir@vladimir-u32.(none)>
2012-12-30 21:27:31 -0800

    guessing game

---
+++ guessnumber.py
@@ -0,0 +1,13 @@
+import random
+number=random.randint(1,20)
+print ("guess a number")
+while(1):
+	
+	guess=input()
+	if number>guess:
+		print("number is bigger than your guess")
+	elif number<guess:
+		print("number is smaller than your guess")		
+	else:
+		print("you won!The number was "+str(number))		
+		break

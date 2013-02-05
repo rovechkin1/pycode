b22384a360a1119c64d5976203607e91229e9888
vladimir ovechkin <vladimir@vladimir-S400.(none)>
2013-02-04 20:21:57 -0800

    check letter

--- hangman.py
+++ hangman.py
@@ -1,4 +1,5 @@
-d=[
+import random
+drawings=[
 '''
   +---+
   |   |
@@ -70,10 +71,59 @@ d=[
 '''
 ]
 
-n=0
-while (n<len(d)):
-	print (d[n])
-	n=n+1
+
+
+
+
+
+
+
+
+
+words='''ant baboon badger bat bear beaver camel cat clam
+cobra cougar coyote crow deer dog donkey duck eagle ferret
+fox frog goat goose hawk lion lizard llama mole monkey
+moose mouse mule newt otter owl panda parrot pigeon python
+rabbit ram rat raven rhino salmon seal shark sheep skunk
+sloth snake spider stork swan tiger toad trout turkey
+turtle weasel whale wolf wombat zebra'''
+def random_word (words):
+	cup=words.split()
+	return cup[random.randint(0,len (cup)-1)]
+	
+
+
+
+
+
+def check_letter (guess,underscore,letter):
+	temp=[0]*len(underscore)
+	for i in range (0,len(underscore)):
+		temp[i]=underscore[i]
+	for i in range (0,len(guess)):
+		if guess[i]==letter:
+			temp[i]=letter
+	print(temp)
+	return ''.join(temp)
+
+
+
+
+
+###########main##########
+
+guess=random_word (words)
+print(guess)
+underscore="_"*len (guess)
+while (1):
+	letter=input("guess a letter: ")
+	underscore=check_letter (guess,underscore,letter)
+	print (underscore)	
+#n=0
+#
+#while (n<len(drawings)):
+#	print (drawings[n])
+#	n=n+1
 
 
 
